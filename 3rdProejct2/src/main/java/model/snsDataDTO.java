2311c6f18c7cb758a949cf6ec652ac5c0f6980a7
임대진 <84882267+fineapplepizza@users.noreply.github.com>
2021-08-12 11:19:43 +0900

    웹서버 DAO,DTO,Command패턴 커밋

---
+++ 3rdProejct2/src/main/java/model/snsDataDTO.java
@@ -0,0 +1,54 @@
+package model;
+
+public class snsDataDTO { // SNS데이터 베이스
+	
+	private int jongmokCode; //종목코드
+	private String emotion; //긍정,부정,무관심
+	private String platForm;//sns종류
+	private String content; //글내용
+	
+	//생성자 메소드
+	public snsDataDTO(int jongmokCode, String emotion, String platForm, String content) {
+		this.jongmokCode = jongmokCode;
+		this.emotion = emotion;
+		this.platForm = platForm;
+		this.content = content;
+	}
+
+	public int getJongmokCode() {//종목코드
+		return jongmokCode;
+	}
+
+	public void setJongmokCode(int jongmokCode) {
+		this.jongmokCode = jongmokCode;
+	}
+
+	public String getEmotion() {//긍정,부정,무관심
+		return emotion;
+	}
+
+	public void setEmotion(String emotion) {
+		this.emotion = emotion;
+	}
+
+	public String getPlatForm() {//SNS종류
+		return platForm;
+	}
+
+	public void setPlatForm(String platForm) {
+		this.platForm = platForm;
+	}
+
+	public String getContent() {//글 내용
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+
+}
