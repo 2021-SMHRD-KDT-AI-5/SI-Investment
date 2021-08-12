7eb1b50f1ab86897663b787b8b9c51f3dccc9a55
임대진 <84882267+fineapplepizza@users.noreply.github.com>
2021-08-12 16:41:00 +0900

    resultURI 경로 수정 및 회원가입,로그인 콘트롤러 생성

--- 3rdProejct2/src/main/java/frontcontroller/frontcontroller.java
+++ 3rdProejct2/src/main/java/frontcontroller/frontcontroller.java
@@ -8,10 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import command.Command;
+import controller_command.joinCon;
+import controller_command.loginCon;
 
 @WebServlet("*.do")//*가 붙으면 어떤이름이든 .do로끝나면 오게된다.
 public class frontcontroller extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//한글 인코딩
@@ -25,17 +27,27 @@ public class frontcontroller extends HttpServlet {
 		String path = request.getContextPath();
 		System.out.println("프로젝트 이름 : "+path);
 		
+	
 		//substring()
 		//값을 두개를 줘서 중간에 값을 짜르는 것 
 		//하나의 값을 줘서 짜른ㄴ것(시작하는 인덱스만지정하고 끝까지)
-		String resultURI = reqURI.substring(path.length()+1);
-		System.out.println("요청 : "+ resultURI);
+		//프로젝트이름이 빠짐
+		String middleURI = reqURI.substring(path.length()+1);
+		System.out.println("요청 : "+ middleURI);
+		
+		//split함수로 요청주소값을 나눔 
+		String[] tmpStringarray = middleURI.split("/");
+		//pages폴더이름이 빠짐
+		String resultURI = tmpStringarray[tmpStringarray.length-1];
+		System.out.println("test: " + resultURI);
 		
 		Command command = null; //Command타입의 변수 선언 
 		
 		if(resultURI.equals("joinCon.do")) {//회원가입
+			command = new joinCon();
 			
 		}else if(resultURI.equals("loginCon.do")){//로그인 
+			command = new loginCon();
 			
 		}else if(resultURI.equals("JugaData.do")) {//주가데이터
 			
