95ab4d7e4f1ad47d7eeb3658c1f90aa130ce5587
임대진 <84882267+fineapplepizza@users.noreply.github.com>
2021-08-24 09:21:33 +0900

    JugaData -> StockBestDAO로 파일명을 바꿈

---
+++ 3rdProejct2/src/main/java/model/StockBestDAO.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+
+public class StockBestDAO {//국내 종목 실시간 BEST
+	
+	public void connection() {//데이터베이스연결
+		
+	}
+	
+	public void close() {//종료
+		
+	}
+	
+	public ArrayList<StockBestDTO> select(){//주가데이터 
+	
+	//ArrayList생성
+    ArrayList<StockBestDTO> list = new ArrayList<StockBestDTO>();
+	
+	StockBestDTO juga = null;
+	
+	return list;
+	}
+
+
+}
