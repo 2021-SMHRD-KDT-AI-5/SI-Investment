614e24860ba20267344f0d2b359d3d543a1c4851
임대진 <84882267+fineapplepizza@users.noreply.github.com>
2021-08-24 22:48:38 +0900

    예측급등률 DAO생성

--- 3rdProejct2/src/main/java/model/statDataDAO.java
+++ 3rdProejct2/src/main/java/model/statDataDAO.java
@@ -59,7 +59,9 @@ public class statDataDAO {//종목별 통계 데이터 DAO
 		
 	}
 	
-	public ArrayList<statDataDTO> select(){//투자별 거래량
+	
+
+public ArrayList<statDataDTO> select(){//투자별 거래량
 		
 		//ArrayList생성
 		ArrayList<statDataDTO> list = new ArrayList<statDataDTO>();
@@ -109,4 +111,53 @@ public class statDataDAO {//종목별 통계 데이터 DAO
 		
 	}
 
+public ArrayList<statDataDTO> soaringRate_select(){//예측급등률
+	
+	//ArrayList생성
+	ArrayList<statDataDTO> list = new ArrayList<statDataDTO>();
+	
+	statDataDTO soaring = null;
+	
+	try {
+		//DB연결 가능
+	    connection();
+		//쿼리 실행
+		String sql = "select * soaring_table order by soaringRate desc limit 4";
+		// 내림차순으로 정렬후 0번째부터4번째까지 출력 sql문
+		
+		//자바 와 DB에 sql문을 실행시켜주는 역할
+		psmt = conn.prepareStatement(sql);
+		
+        
+		//★★★★★
+		//insert,update,delete: executeUpdate() --> DB에 내용을 변경할 때
+		//select: executeQuery() --> DB에 내용을 검색할 때
+		rs = psmt.executeQuery();
+		System.out.println(sql);
+		//여러번 확인해야 하기 때문에 while
+		while(rs.next()) {
+			//컬럼인덱스는 1부터 시작
+			int getJongmokCode = rs.getInt(1);
+			String getJongmokName = rs.getString(2);
+			int getSoaringRate = rs.getInt(3);
+			
+			
+		    soaring = new statDataDTO(getJongmokCode,getJongmokName,getSoaringRate);
+			//list안에 각각의  예측 급등률 정보가 담겨있는 soaring 추가
+			list.add(soaring);
+			
+		}
+	
+	}  catch( SQLException e) {
+	
+		System.out.println("sql문 오류다.");
+		e.printStackTrace();
+	} finally {
+	    close();
+	}//end
+	
+	
+	return list;
+	
+}
 }
