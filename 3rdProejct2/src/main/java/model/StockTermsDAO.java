ed2de127494cea0ac3878a46a4fcc98baabcd21a
임대진 <84882267+fineapplepizza@users.noreply.github.com>
2021-08-24 09:25:57 +0900

    TermDicDAO->StockTermDAO로 파일명 바꿈

---
+++ 3rdProejct2/src/main/java/model/StockTermsDAO.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class StockTermsDAO {//주식용어 사전
+	
+	public void connection() {//데이터베이스
+		
+	}
+	
+	public void close() {//종료
+		
+	}
+
+	public ArrayList<StockTermsDTO> select(){//주식용어사전
+		
+		//ArrayList생성
+		ArrayList<StockTermsDTO>list = new ArrayList<StockTermsDTO>();
+
+		StockTermsDTO termDic = null;
+		
+		
+		return list;
+	}
+}
